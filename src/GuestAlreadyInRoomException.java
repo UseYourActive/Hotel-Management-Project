a1b8faaa6a0434ed99c627a67cca837a481ee702
Alex Orozov <alexorozov@gmail.com>
2023-03-21 14:22:11 +0200

    Добавяме още един конструктор към класа ни за грешки, да имаме.

--- src/GuestAlreadyInRoomException.java
+++ src/GuestAlreadyInRoomException.java
@@ -6,6 +6,10 @@ public class GuestAlreadyInRoomException extends Exception
     {
         super(message);
     }
+    GuestAlreadyInRoomException(Throwable cause)
+    {
+        super(cause);
+    }
 
     GuestAlreadyInRoomException(String message, Throwable cause)
     {
