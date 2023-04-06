047fc1ad22efc5d6ef73f955d03240e30ea6844a
Alex Orozov <alexorozov@gmail.com>
2023-04-06 14:42:22 +0300

    Moved guest to a separate package

---
+++ src/guest/Guest.java
@@ -0,0 +1,129 @@
+package guest;
+
+public class Guest
+{
+    /* Members of the class. */
+    private long id;
+    private static long objectCounter;
+    private String firstName;
+    private String secondName;
+    private String lastName;
+    private String uniqueIdentificationNumber; // ЕГН
+    private String phoneNumber;
+    /*----------------------------------------------------------------------*/
+
+    /* Builder of the class. */
+    public static class Builder
+    {
+        private long id;
+        private static long objectCounter;
+        private final String firstName;
+        private final String secondName;
+        private final String lastName;
+        private final String uniqueIdentificationNumber; // ЕГН
+        private final String phoneNumber;
+
+        public Builder(String firstName, String secondName, String lastName, String uniqueIdentificationNumber, String phoneNumber)
+        {
+            this.firstName = firstName;
+            this.secondName = secondName;
+            this.lastName = lastName;
+            this.uniqueIdentificationNumber = uniqueIdentificationNumber;
+            this.phoneNumber = phoneNumber;
+            objectCounter++;
+            this.id = objectCounter;
+        }
+
+        public Builder withID(long id)
+        {
+            this.id = id;
+            return this;
+        }
+        public Guest build()
+        {
+            return new Guest(firstName,secondName,lastName,uniqueIdentificationNumber,phoneNumber);
+        }
+    }
+    /*----------------------------------------------------------------------*/
+
+    /* Constructor of the class. */
+    private Guest(String firstName, String secondName, String lastName, String uniqueIdentificationNumber, String phoneNumber)
+    {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.lastName = lastName;
+        this.uniqueIdentificationNumber = uniqueIdentificationNumber;
+        this.phoneNumber = phoneNumber;
+        objectCounter++;
+        this.id = objectCounter;
+    }
+    /*----------------------------------------------------------------------*/
+
+    /* Methods of the class. */
+    /*----------------------------------------------------------------------*/
+
+    /* Overrides. */
+    @Override
+    public String toString()
+    {
+       StringBuilder stringBuilder = new StringBuilder();
+
+       stringBuilder.append("ID: ")                         .append(this.id)                             .append("\n");
+       stringBuilder.append("First name: ")                 .append(this.firstName)                      .append("\n");
+       stringBuilder.append("Second name: ")                .append(this.secondName)                     .append("\n");
+       stringBuilder.append("Last name: ")                  .append(this.lastName)                       .append("\n");
+       stringBuilder.append("Identification number: ")      .append(this.uniqueIdentificationNumber)     .append("\n");
+       stringBuilder.append("Phone number: ")               .append(this.phoneNumber)                    .append("\n");
+
+       return String.valueOf(stringBuilder);
+    }
+    /*----------------------------------------------------------------------*/
+
+    /* Accessors and Mutators of the class. */
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public String getSecondName()
+    {
+        return secondName;
+    }
+    public String getLastName()
+    {
+        return lastName;
+    }
+    public String getUniqueIdentificationNumber()
+    {
+        return uniqueIdentificationNumber;
+    }
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+    public void setSecondName(String secondName)
+    {
+        this.secondName = secondName;
+    }
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+    public void setUniqueIdentificationNumber(String uniqueIdentificationNumber)
+    {
+        this.uniqueIdentificationNumber = uniqueIdentificationNumber;
+    }
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+    /*----------------------------------------------------------------------*/
+}
