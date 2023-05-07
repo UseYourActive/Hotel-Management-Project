21810534a57ea9022875a0fb0e06160f1b899f35
Alex Orozov <alexorozov@gmail.com>
2023-05-07 12:16:58 +0300

    Updating the Hotel class.

--- src/models/hotel/Hotel.java
+++ src/models/hotel/Hotel.java
@@ -1,15 +1,15 @@
-package hotel;
+package models.hotel;
 
-import guest.Guest;
-import rooms.*;
+import models.rooms.*;
 import errorlogger.ErrorLogWriter;
 import errorlogger.ErrorWritable;
 import exceptions.InvalidRoomTypeException;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.*;
 
+@XmlRootElement
 public class Hotel {
     private static List<Room> hotelRooms;
     private static Hotel instance;
@@ -26,10 +26,9 @@ public class Hotel {
             hotelName = "Orozov's Brothel";
             errorLogger = new ErrorLogWriter();
             factory = new RoomFactory();
-            return instance;
-        } else{
-            return instance;
+            Hotel.initialize();
         }
+        return instance;
     }
 
 
@@ -38,61 +37,47 @@ public class Hotel {
 //    да се извежда списък на всички записали се за дадена дейност
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public String showAllRooms() {
         StringBuilder allRooms = new StringBuilder();
 
         for (Room room : hotelRooms) {
-            allRooms.append(room.displayInfo()).append("\n");
+            allRooms.append(room.toString()).append("\n");
         }
 
         return String.valueOf(allRooms);
     }
 
-    public void initialize() {
+    private static void initialize() {
         int NUMBER_OF_HOTEL_ROOM_INITIALIZATIONS = 5;
         int ROOM_INCREMENT = 1;
 
         for(int index = 1; index < NUMBER_OF_HOTEL_ROOM_INITIALIZATIONS + ROOM_INCREMENT; index++) {
-            try
-            {
+            try {
                 hotelRooms.add(factory.createRoom(RoomTypes.SINGLE_ROOM));
                 hotelRooms.add(factory.createRoom(RoomTypes.DOUBLE_ROOM));
                 hotelRooms.add(factory.createRoom(RoomTypes.DELUXE_ROOM));
                 hotelRooms.add(factory.createRoom(RoomTypes.PRESIDENT_ROOM));
-            } catch (InvalidRoomTypeException e)
-            {
+            } catch (InvalidRoomTypeException e) {
                 errorLogger.writeToErrorLog(e);
             }
         }
     }
 
-    // add rooms or initialize them to be already in the list!
-
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
 
-        stringBuilder.append("Hotel name: ").append(this.hotelName).append("\n");
+        stringBuilder.append("Hotel name: ").append(hotelName).append("\n");
 
         for(Room room : hotelRooms) {
             stringBuilder.append(room.toString()).append("\n");
         }
 
         return String.valueOf(stringBuilder);
+    }
+
+    @XmlElement(name = "room")
+    public static List<Room> getHotelRooms() {
+        return new ArrayList<>(hotelRooms);
     }
 }
