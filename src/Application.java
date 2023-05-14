44582c9cf4dd0d4d5b9e10e3bbe512017366f174
Alex Orozov <alexorozov@gmail.com>
2023-05-14 23:56:45 +0300

    Creating an application class with a static method run. For testing purposes.

---
+++ src/Application.java
@@ -0,0 +1,60 @@
+import commandlineinterface.factories.DefaultCommandFactory;
+import commandlineinterface.enums.DefaultCommands;
+import utils.services.HotelService;
+import models.Hotel;
+import models.hotelcommands.enums.HotelCommands;
+import models.hotelcommands.HotelCommandsFactory;
+import utils.errorlogger.ErrorLogWriter;
+import exceptions.commands.InvalidCommandException;
+import utils.parsers.xml.JAXBParser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Application {
+    public static void run(){
+        Scanner scanner = new Scanner(System.in);
+        ErrorLogWriter errorLogWriter = new ErrorLogWriter();
+        JAXBParser jaxbParser = new JAXBParser();
+        Hotel hotel = Hotel.getInstance(errorLogWriter);
+        HotelService hotelService = new HotelService(errorLogWriter, jaxbParser);
+        DefaultCommandFactory commandFactory = new DefaultCommandFactory(hotelService, errorLogWriter);
+        HotelCommandsFactory hotelCommandsFactory = new HotelCommandsFactory(hotelService, errorLogWriter);
+
+        String input;
+        List<String> arguments;
+        Enum<?> command;
+
+        while(true){
+            input = scanner.nextLine().toLowerCase();
+            arguments = new ArrayList<>(List.of(input.split(" ")));
+            try {
+                command = commandParsingMethod(arguments.get(0));
+                if(command instanceof DefaultCommands){
+                    commandFactory.createCommand((DefaultCommands) command, arguments.subList(1, arguments.size())).execute();
+                }else if(command instanceof HotelCommands){
+                    hotelCommandsFactory.createCommand((HotelCommands) command, arguments.subList(1, arguments.size())).execute();
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static Enum<?> commandParsingMethod(String command) throws InvalidCommandException {
+        for(DefaultCommands value : DefaultCommands.values()) {
+            if (value.getCommand().equals(command)) {
+                return DefaultCommands.valueOf(command.toUpperCase());
+            }
+        }
+
+        for(HotelCommands value : HotelCommands.values()) {
+            if (value.getChoice().equals(command)) {
+                return HotelCommands.valueOf(command.toUpperCase());
+            }
+        }
+
+        throw new InvalidCommandException(command + " is not recognized as an internal command!");
+    }
+}
