86142f7c3a1f1bf465737b05fe6ecaf66cfa1cdd
Alex Orozov <alexorozov@gmail.com>
2023-05-15 00:04:20 +0300

    Creating a FileParser for possible multi usage for other parsers.

---
+++ src/utils/parsers/FileParser.java
@@ -0,0 +1,13 @@
+package utils.parsers;
+
+import models.Hotel;
+
+import javax.xml.bind.JAXBException;
+import java.io.File;
+
+public interface FileParser <T> {
+    T readFromFile(File file) throws JAXBException;
+    void writeToFile(Hotel hotel, File file) throws JAXBException;
+    void createFileIfNotExist(Hotel hotel, File file) throws JAXBException;
+    boolean deleteFile(File file);
+}
